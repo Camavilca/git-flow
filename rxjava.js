cd615eb40df0aca097480ec0662c7488bd769bd0
Orlando Camavilca Chavez <FU72080811-EXT@somosoh.pe>
2023-02-04 22:24:05 -0500

    edad

--- rxjava.js
+++ rxjava.js
@@ -1,9 +1,10 @@
 var nombre = "orlando";
 var direccion = "carhuamayo";
-
+var edad = 12;
 
 var resultado = function suma(x, y) {
     return x + y;
 }
 
 console.log(resultado);
+console.log(edad);
